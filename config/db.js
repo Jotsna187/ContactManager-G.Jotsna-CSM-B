6dccf116a934ab9b0c70cc76331c042091fcac28
Jotsna187 <priya.galinki07@gmail.com>
2026-07-13 14:40:45 +0530

    Update db.js

--- config/db.js
+++ config/db.js
@@ -1,7 +1,9 @@
 const mongoose = require("mongoose");
+const dns = require("dns")
+dns.setServers(["8.8.8.8","8.8.4.4"])
 const connectDB = async () => {
     try {
-        await mongoose.connect("mongodb://127.0.0.1:27017/ContactManager");
+        await mongoose.connect("mongodb+srv://priyagalinki:<db_password>@cluster0.byx2ils.mongodb.net/?appName=Cluster0==");
         console.log("MongoDB Connected Successfully");
     } catch (error) {
         console.log(error.message);
