6f0ba64dc7c8b904882864f40d012e7303cabc6e
Jotsna187 <priya.galinki07@gmail.com>
2026-07-14 10:43:24 +0530

    Update MongoDB connection string with new credentials

--- config/db.js
+++ config/db.js
@@ -3,7 +3,7 @@ const dns = require("dns")
 dns.setServers(["8.8.8.8","8.8.4.4"])
 const connectDB = async () => {
     try {
-        await mongoose.connect("mongodb+srv://priyagalinki:<db_password>@cluster0.byx2ils.mongodb.net/?appName=Cluster0==");
+        await mongoose.connect("mongodb+srv://priyagalinki_db_user:Jotsna@123@cluster0.byx2ils.mongodb.net/?appName=Cluster0");
         console.log("MongoDB Connected Successfully");
     } catch (error) {
         console.log(error.message);
