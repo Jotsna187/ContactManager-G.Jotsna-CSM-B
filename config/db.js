7b4152e9a6c9983f74afb6f74bce0654c5b6ebf3
Jotsna187 <priya.galinki07@gmail.com>
2026-07-14 12:04:42 +0530

    Update MongoDB connection string for Contactmanager

--- config/db.js
+++ config/db.js
@@ -3,7 +3,7 @@ const dns = require("dns")
 dns.setServers(["8.8.8.8","8.8.4.4"])
 const connectDB = async () => {
     try {
-        await mongoose.connect("mongodb+srv://priyagalinki_db_user:jotsna123@cluster0.byx2ils.mongodb.net/?appName=Cluster0");
+        await mongoose.connect("mongodb+srv://priyagalinki_db_user:jotsna123@cluster0.byx2ils.mongodb.net/Contactmanager?appName=Cluster0");
         console.log("MongoDB Connected Successfully");
     } catch (error) {
         console.log(error.message);
